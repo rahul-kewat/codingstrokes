c66cb4053104e54254f8541e4a5f1ac27999184b
rahul <rahul97815kewat@gmail.com>
2020-04-07 22:27:52 +0530

    home page updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,11 +14,24 @@
                             {{ session('status') }}
                         </div>
                 @endif
-            <h3>We are currently in development phase. Please give us some time. Till then enjoy the articles below</h3><br>
-            <h5>Latest Articles :</h5> <br><br>
             
             @foreach($articles as $article)
-                <a href='{!! $article->urldest !!}' ><h4>{!! $article->title !!}</h4></a>
+            <div class="media bg-light position-relative " style="border-radius:1%; padding:2%">
+                <div class="media-body">
+                    <div class="row">
+                        <div class="col-sm-1">
+                        <img src="https://camo.githubusercontent.com/f60e683c1994c1011fa1418e789a998fb4b161aa/68747470733a2f2f692e696d6775722e636f6d2f5361466a6d65742e706e67" height="15px" class="mr-3" alt="...">
+                        <b>Laravel</b>
+                        </div>
+                        <div class="col-sm-11">
+                            <h5 class="mt-0 articleListHeading"><b>{!! $article->title !!}</b></h5>
+                            <p class="articleListPara">{!! str_limit($article->content, 200) !!}</p>
+                            <a href="{!! $article->urldest !!}" class="stretched-link"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <a href='{!! $article->urldest !!}' ><h4></h4></a>
             @endforeach
                 
         </div>
