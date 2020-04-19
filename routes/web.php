48a7fe2a5abc55b5fed646ec5cad2b8ab9662b5f
rahul <rahul97815kewat@gmail.com>
2020-04-19 10:53:01 +0530

    web.php incoming changes issue fixed

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
-<<<<<<< HEAD
-use App\Models\DailyQuote;
-=======
 
->>>>>>> 288c2cab575d4a8f6fc86da01d9f4063a88ac67e
+use App\Models\DailyQuote;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,18 +15,18 @@ use App\Models\DailyQuote;
 */
 
 Route::get('/', function () {
-<<<<<<< HEAD
+
     $dailyquote = DailyQuote::inRandomOrder()->limit(1)->first();
     return view('homesearch',compact('dailyquote'));
-=======
+
     return view('welcome');
->>>>>>> 288c2cab575d4a8f6fc86da01d9f4063a88ac67e
+
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-<<<<<<< HEAD
+
 
 // Show Article 
 Route::get('/article/{tag}/{id}-{slug}', 'ArticleController@show');
@@ -38,6 +37,4 @@ Route::get('/user/{id}', 'UsersController@view');
 // Search
 Route::get('/search', 'SearchController@index');
 
-=======
 Route::get('/article/{tag}/{id}-{slug}', 'ArticleController@show');
->>>>>>> 288c2cab575d4a8f6fc86da01d9f4063a88ac67e
